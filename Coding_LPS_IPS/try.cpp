2a088926be691dbf671d52b1f966f4e1dd3d2c51
Rumman <rummanfardeen4567@gmail.com>
2023-05-24 15:42:47 +0530

    Files removed

--- Coding_LPS_IPS/try.cpp
+++ Coding_LPS_IPS/try.cpp
@@ -1,110 +1,97 @@
 #include <iostream>
 using namespace std;
 
-void insertionSort(int arr[], int left, int right)
+void merge(float arr[], int low, int mid1, int mid2, int high)
 {
-    for (int i = left + 1; i <= right; ++i)
+    int n1 = mid1 - low + 1;
+    int n2 = mid2 - mid1;
+    int n3 = high - mid2;
+
+    float left[n1], middle[n2], right[n3];
+
+    for (int i = 0; i < n1; i++)
+        left[i] = arr[low + i];
+    for (int i = 0; i < n2; i++)
+        middle[i] = arr[mid1 + 1 + i];
+    for (int i = 0; i < n3; i++)
+        right[i] = arr[mid2 + 1 + i];
+
+    int i = 0, j = 0, k = 0, p = low;
+
+    while (i < n1 && j < n2 && k < n3)
     {
-        int key = arr[i];
-        int j = i - 1;
-        while (j >= left && arr[j] > key)
+        if (left[i] <= middle[j] && left[i] <= right[k])
         {
-            arr[j + 1] = arr[j];
-            --j;
+            arr[p] = left[i];
+            i++;
         }
-        arr[j + 1] = key;
-    }
-}
-
-void merge(int arr[], int left, int mid, int right)
-{
-    int n1 = mid - left + 1;
-    int n2 = right - mid;
-
-    int leftArr[n1];
-    int rightArr[n2];
-
-    for (int i = 0; i < n1; ++i)
-    {
-        leftArr[i] = arr[left + i];
-    }
-    for (int i = 0; i < n2; ++i)
-    {
-        rightArr[i] = arr[mid + 1 + i];
-    }
-
-    int i = 0;
-    int j = 0;
-    int k = left;
-
-    while (i < n1 && j < n2)
-    {
-        if (leftArr[i] <= rightArr[j])
+        else if (middle[j] <= left[i] && middle[j] <= right[k])
         {
-            arr[k] = leftArr[i];
-            ++i;
+            arr[p] = middle[j];
+            j++;
         }
         else
         {
-            arr[k] = rightArr[j];
-            ++j;
+            arr[p] = right[k];
+            k++;
         }
-        ++k;
+        p++;
     }
 
     while (i < n1)
     {
-        arr[k] = leftArr[i];
-        ++i;
-        ++k;
+        arr[p] = left[i];
+        i++;
+        p++;
     }
 
     while (j < n2)
     {
-        arr[k] = rightArr[j];
-        ++j;
-        ++k;
+        arr[p] = middle[j];
+        j++;
+        p++;
+    }
+
+    while (k < n3)
+    {
+        arr[p] = right[k];
+        k++;
+        p++;
     }
 }
 
-void modifiedMergeSort(int arr[], int left, int right)
+void mergeSort(float arr[], int low, int high)
 {
-    if (left < right)
+    if (low < high)
     {
-        if (right - left + 1 <= 3)
-        {
-            insertionSort(arr, left, right);
-        }
-        else
-        {
-            int mid = left + (right - left) / 2;
-            modifiedMergeSort(arr, left, mid);
-            modifiedMergeSort(arr, mid + 1, right);
-            merge(arr, left, mid, right);
-        }
+        int mid1 = low + (high - low) / 3;
+        int mid2 = low + 2 * (high - low) / 3;
+
+        mergeSort(arr, low, mid1);
+        mergeSort(arr, mid1 + 1, mid2);
+        mergeSort(arr, mid2 + 1, high);
+
+        merge(arr, low, mid1, mid2, high);
     }
+}
+
+void printArray(float arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << "\n";
 }
 
 int main()
 {
-
     int n;
     cin >> n;
-    int arr[n];
+
+    float arr[n];
     for (int i = 0; i < n; ++i)
     {
         cin >> arr[i];
     }
-
-    modifiedMergeSort(arr, 0, n - 1);
-
-    // Print the sorted array
-    std::cout << "Sorted array:\n";
-    for (int i = 0; i < n; ++i)
-    {
-        std::cout << arr[i] << "\n";
-    }
-    std::cout << "\n";
-
+    mergeSort(arr, 0, n - 1);
+    printArray(arr, n);
     return 0;
 }
