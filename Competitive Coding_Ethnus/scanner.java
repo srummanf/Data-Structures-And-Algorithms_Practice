b45241d3f460dcb5912b9738245eb0bfd75b1a66
Rumman <rummanfardeen4567@gmail.com>
2023-05-27 12:52:44 +0530

    For Loop Program added

--- Competitive Coding_Ethnus/scanner.java
+++ Competitive Coding_Ethnus/scanner.java
@@ -1,13 +1,14 @@
-import java.util.*; 
-public class scanner {   
-public static void main(String args[]) {                      
-   		String str = "Codemithra/is/a/learning/platform/"; 
-    		Scanner sc = new Scanner(str);           
-    sc.useDelimiter("/"); 
-         System.out.println("---Result---");  
-         while(sc.hasNext()) { 
-         	System.out.println(sc.next()); 
-         } 
-   System.out.println("Delimiter used: " +sc.delimiter());           
-   	 }   
- }
+import java.util.*;
+
+public class scanner {
+    public static void main(String args[]) {
+        String str = "Codemithra/is/a/learning/platform/";
+        Scanner sc = new Scanner(str);
+        sc.useDelimiter("/");
+        System.out.println("---Result---");
+        while (sc.hasNext()) {
+            System.out.println(sc.next());
+        }
+        System.out.println("Delimiter used: " + sc.delimiter());
+    }
+}
