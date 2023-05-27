275a6fe2eadf5f015e46e3bda4745e70fdef602f
Rumman <rummanfardeen4567@gmail.com>
2023-05-27 16:35:26 +0530

    Euler Phi added

--- Competitive Coding_Ethnus/eulerphi_algo.java
+++ Competitive Coding_Ethnus/eulerphi_algo.java
@@ -2,11 +2,22 @@ import java.util.*;
 
 public class eulerphi_algo {
     int gcd(int a, int b){
-        if(b==0)
+        if(a==0)
         return (b);
         else
-        return gcd(a%b,b);
+        return gcd(b%a,a);
     }
 
-    if()
+    public static void main(String[] args)
+    {
+        eulerphi_algo ob = new eulerphi_algo();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int res = 0;
+        for(int i=1;i<n;i++){
+            if(ob.gcd(n,i)==1)
+            res++;
+        }
+        System.out.println(res);
+    }
 }
