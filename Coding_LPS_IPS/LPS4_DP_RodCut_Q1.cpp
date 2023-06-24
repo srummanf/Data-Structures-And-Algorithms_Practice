e43e1cafbacb24997a6f2a75f51ed416ec7686f4
Rumman <rummanfardeen4567@gmail.com>
2023-06-24 09:27:28 +0530

    DP Programs added

--- Coding_LPS_IPS/LPS4_DP_RodCut_Q1.cpp
+++ Coding_LPS_IPS/LPS4_DP_RodCut_Q1.cpp
@@ -1,4 +1,4 @@
-// A variation of rod cutting problem is one in which length of the rod will be of length greater than or equal to 8 and you can make cuts of rods of length 3 or 5 only.For each unit of wastage a penalty of Rs .1 should be given.Write an recursive algorithm and implement it to find the maximum revenue that may be generated.
+// A variation of rod cutting problem is one in which length of the rod will be of length greater than or equal to 8 and you can make cuts of rods of length 3 or 5 only.For each unit of wastage a penalty of Rs .1 should be given. Write an recursive algorithm and implement it to find the maximum revenue that may be generated.
 // Input Format
 
 // First line contains the length of the rod,
