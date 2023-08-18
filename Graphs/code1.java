b0705e81c2c0f5b52be0a027eed0948a90ed7c19
Rumman <rummanfardeen4567@gmail.com>
2023-08-18 15:18:54 +0530

    Programs added

--- Graphs/code1.java
+++ Graphs/code1.java
@@ -38,7 +38,7 @@ public class code1 {
     // print 2's neighbours
     for(int i = 0; i < graph[2].size(); i++){
       Edge e = graph[2].get(i);
-      System.out.println(e.src + " --> " + e.dest);
+      System.out.println(e.src + " --> " + e.dest + " with weight " + e.weight);
     }
   }
 }
