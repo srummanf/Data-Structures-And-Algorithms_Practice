78b07ffd3c2b15e1d5a6e7228d62b60c7b8b4b24
Rumman <rummanfardeen4567@gmail.com>
2023-08-21 15:59:50 +0530

    BFS added

--- Graphs/code1.java
+++ Graphs/code1.java
@@ -1,9 +1,11 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class code1 {
 
   static class Edge {
+
     int src, dest, weight;
+
     public Edge(int s, int d, int w) {
       src = s;
       dest = d;
@@ -16,7 +18,7 @@ public class code1 {
       graph[i] = new ArrayList<Edge>();
     }
 
-    graph[0].add(new Edge(0, 2,2));
+    graph[0].add(new Edge(0, 2, 2));
 
     graph[1].add(new Edge(1, 2, 10));
     graph[1].add(new Edge(1, 3, 0));
@@ -29,6 +31,28 @@ public class code1 {
     graph[3].add(new Edge(3, 2, -1));
   }
 
+  public static void BFS(
+    ArrayList<Edge> graph[],
+    int V,
+    boolean visited[],
+    int start
+  ) {
+    Queue<Integer> q = new LinkedList<>();
+
+    q.add(start);
+    while (!q.isEmpty()) {
+      int curr = q.remove();
+      if (visited[curr] == false) {
+        System.out.print(curr + " ");
+        visited[curr] = true;
+        for (int i = 0; i < graph[curr].size(); i++) {
+          Edge e = graph[curr].get(i);
+          q.add(e.dest);
+        }
+      }
+    }
+  }
+
   public static void main(String[] args) {
     int V = 4;
     ArrayList<Edge> graph[] = new ArrayList[V];
@@ -36,9 +60,15 @@ public class code1 {
     createGraph(graph);
 
     // print 2's neighbours
-    for(int i = 0; i < graph[2].size(); i++){
+    for (int i = 0; i < graph[2].size(); i++) {
       Edge e = graph[2].get(i);
       System.out.println(e.src + " --> " + e.dest + " with weight " + e.weight);
+    }
+    boolean visited[] = new boolean[V];
+    for (int i = 0; i < V; i++) {
+      if (visited[i] == false) {
+        BFS(graph, V, visited, i);
+      }
     }
   }
 }
