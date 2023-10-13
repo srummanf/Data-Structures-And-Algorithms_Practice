552009b70f1ab964fccdff850f03bc29bce4baba
Rumman <rummanfardeen4567@gmail.com>
2023-10-13 11:44:04 +0530

    ✨Kth Bit Set Program added✨

---
+++ Bitwise Ops/FloorDivisionby2powerk.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class FloorDivisionby2powerk {
+    int div(int n, int k){
+        return n>>k;
+    }
+    public static void main(String[] args){
+        FloorDivisionby2powerk obj = new FloorDivisionby2powerk();
+        System.out.println(obj.div(10, 2));
+    }
+}
