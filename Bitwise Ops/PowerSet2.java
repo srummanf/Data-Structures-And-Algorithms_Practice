256bf351552aface179970dd72c146ddc7b590a6
Rumman <rummanfardeen4567@gmail.com>
2023-10-13 11:34:36 +0530

    ✨PowerSet using backtracking Program added✨

---
+++ Bitwise Ops/PowerSet2.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class PowerSet2 {
+
+    ArrayList<String> powerSetList = new ArrayList<>(); // Create an ArrayList to store the power set
+
+    void generatePowerSet(String str, int index, String current) {
+        int n = str.length();
+
+        // If we've reached the end of the input string, add the current subset to the list
+        if (index == n) {
+            powerSetList.add(current);
+            return;
+        }
+
+        // Include the current character in the subset and recurse
+        generatePowerSet(str, index + 1, current + str.charAt(index));
+
+        // Exclude the current character from the subset and recurse
+        generatePowerSet(str, index + 1, current);
+    }
+
+    public static void main(String args[]) {
+        PowerSet2 obj = new PowerSet2();
+        String str = "abc";
+
+        // Start generating the power set from an empty string and index 0
+        obj.generatePowerSet(str, 0, "");
+
+        // Now, obj.powerSetList contains the power set of the string "abc"
+        // You can access the subsets using obj.powerSetList
+        for (String subset : obj.powerSetList) {
+            System.out.println(subset);
+        }
+    }
+}
