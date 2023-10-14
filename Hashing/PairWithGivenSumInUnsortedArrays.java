079bb9e63e41c324b90ef17d495356373b7370d8
Rumman <rummanfardeen4567@gmail.com>
2023-10-14 12:45:20 +0530

    ✨Pair With Given Sum  Program added✨

---
+++ Hashing/PairWithGivenSumInUnsortedArrays.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class PairWithGivenSumInUnsortedArrays {
+    boolean check(int arr[], int sum){
+        HashSet<Integer> h = new HashSet<>();
+        
+        for(int x : arr){
+            if(h.contains(sum - x)){
+                return true;
+            }else{
+                h.add(x);
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int arr[] = {3, 2, 8, 15, -8};
+        int sum = 17;
+        PairWithGivenSumInUnsortedArrays p = new PairWithGivenSumInUnsortedArrays();
+        System.out.println(p.check(arr, sum));
+    }
+}
