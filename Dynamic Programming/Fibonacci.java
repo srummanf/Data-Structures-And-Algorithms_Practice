f7ed52ec53f9af270f800729e20db2707ae83ebf
Rumman <rummanfardeen4567@gmail.com>
2023-10-19 12:34:35 +0530

    ✨Fibonacci Sum Bottom up✨

--- Dynamic Programming/Fibonacci.java
+++ Dynamic Programming/Fibonacci.java
@@ -20,3 +20,11 @@ class Fibonacci {
     System.out.println(f.fib(n, dp));
   }
 }
+
+
+// For tabulation  --> Bottom Up
+// dp[0] = 0;
+// dp[1] = 1;
+// for (int i = 2; i <= n; i++) {
+//   dp[i] = dp[i - 1] + dp[i - 2];
+// }
