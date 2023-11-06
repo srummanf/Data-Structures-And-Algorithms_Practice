7adfcb04417c4dc72161c1c9a0ba606054d5c357
Rumman <rummanfardeen4567@gmail.com>
2023-11-06 11:15:06 +0530

    ✨Learn PQ added✨

---
+++ Heaps/Learn_PQ.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Learn_PQ {
+
+  public static void main(String[] args) {
+    PriorityQueue<Integer> pq = new PriorityQueue<>();
+    pq.offer(4);
+    pq.offer(2);
+    pq.offer(1);
+    pq.offer(3);
+    pq.offer(5);
+    // pq.add(6) also works but in the developers world, usually offer is used in PQ.
+    pq.remove(5);
+    System.out.println(pq);
+    System.out.println(pq.peek());
+    System.out.println("Head of PQ: " + pq.peek());
+    System.out.println("Head removed: " + pq.poll());
+    System.out.println(pq);
+
+    Iterator<Integer> itr = pq.iterator();
+    while (itr.hasNext()) {
+      System.out.println(itr.next());
+    }
+  }
+}
