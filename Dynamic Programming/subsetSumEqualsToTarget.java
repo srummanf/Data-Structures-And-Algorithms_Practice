01980984c010d5e8e939405ba8651e86a8d32b32
Rumman <rummanfardeen4567@gmail.com>
2024-01-07 12:07:42 +0530

    DP and HardLeetcode added 🔨

---
+++ Dynamic Programming/subsetSumEqualsToTarget.java
@@ -0,0 +1,62 @@
+// Recursive solutione
+// boolean f(int ind, int target, int arr[]){
+//     if(target==0) return true;
+//     if(ind==0) return arr[0]==target; --> This will return true if array[0] equal to target
+
+//     boolean nottake = f(ind-1, target, arr);
+//     boolean take = false;
+//     if(a[ind]<= target){
+//         take = f(ind-1, target-a[ind], arr);
+//     }
+
+//     return take || nottake;
+// }
+
+import java.util.Arrays;
+
+public class subsetSumEqualsToTarget {
+
+  boolean f(int ind, int target, int arr[]) {
+    if (target == 0) return true;
+    if (ind == 0) return arr[0] == target;
+
+    boolean nottake = f(ind - 1, target, arr);
+    boolean take = false;
+    if (arr[ind] <= target) {
+      take = f(ind - 1, target - a[ind], arr);
+    }
+
+    return take || nottake;
+  }
+
+  // Memoization
+
+  public static boolean f(int ind, int target, int[] arr, int[][] dp) {
+    // If the target sum is achieved, return true
+    if (target == 0) return true;
+
+    // If we have considered all elements but haven't reached the target, return false
+    if (ind == 0) return arr[0] == target;
+
+    // If the result for this subproblem has already been calculated, return it
+    if (dp[ind][target] != -1) return dp[ind][target] == 0 ? false : true;
+
+    // Try not taking the current element
+    boolean notTaken = f(ind - 1, target, arr, dp);
+
+    // Try taking the current element if it doesn't exceed the target
+    boolean taken = false;
+    if (arr[ind] <= target) taken = f(ind - 1, target - arr[ind], arr, dp);
+
+    // Store the result in the DP table and return whether either option was successful
+    dp[ind][target] = notTaken || taken ? 1 : 0;
+    return notTaken || taken;
+  }
+
+  public static boolean subsetSumToK(int n, int k, int arr[]) {
+    // Write your code here.
+    int dp[][] = new int[n][k + 1];
+    for (int row[] : dp) Arrays.fill(row, -1);
+    return f(n - 1, k, arr, dp);
+  }
+}
---
+++ Misc/Arithmetic_II_Subsequence.java
@@ -0,0 +1,39 @@
+/** This is Leetcode Hard question : 446
+ * Video Solution: https://www.youtube.com/watch?v=cau0JE1q_ZA
+ * Code : https://tutorialcup.com/leetcode-solutions/arithmetic-slices-ii-subsequence-leetcode-solution.htm
+ *
+ * Major Learning: Array of Map, Manipulation of old matrix and how to remember it
+
+ */
+
+
+import java.util.*;
+
+public class Arithmetic_II_Subsequence {
+    public int numOfArithmeticSlices(int[] nums){
+        int n = nums.length;
+        long ans = 0;
+        Map<Integer, Integer>[] frequencyArrayMap = new Map[n];
+        for(int i=0;i<n;i++){
+            frequencyArrayMap[i]=new HashMap<>();
+        }
+        for(int i=0;i<n;i++){
+           for(int  j=0;j<i;j++){
+               long commondiff = (long)nums[i]-(long)nums[j];
+               if(commondiff<Integer.MIN_VALUE || commondiff> Integer.MAX_VALUE)continue;
+               int diff =(int)commondiff;
+               int prevFreq=frequencyArrayMap[j].getOrDefault(diff,0);
+               int currFreq=frequencyArrayMap[i].getOrDefault(diff,0);
+               ans+=prevFreq;
+               frequencyArrayMap[i].put(diff,prevFreq+currFreq+1);
+           }
+        }
+        return (int)ans;
+    }
+
+    public static void main(String[] args) {
+        Arithmetic_II_Subsequence obj = new Arithmetic_II_Subsequence();
+        int[] nums = {2,4,6,8,10};
+        System.out.println(obj.numOfArithmeticSlices(nums));
+    }
+}
