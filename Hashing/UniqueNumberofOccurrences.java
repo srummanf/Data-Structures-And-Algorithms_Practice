a2c4033cddccde90ee27b57cfd91c193ef5cbab8
Rumman <rummanfardeen4567@gmail.com>
2024-01-17 21:25:14 +0530

    haan ji good morning

--- Hashing/UniqueNumberofOccurrences.java
+++ Hashing/UniqueNumberofOccurrences.java
@@ -40,4 +40,22 @@ public class UniqueNumberofOccurrences {
         return true;
         
     }
+// Sol 2
+    public boolean uniqueOccurrences2(int[] arr) {
+        // Create a hashmap to store the counts of each number.
+        Map<Integer, Integer> countMap = new HashMap<>();
+      
+        // Iterate over the array and populate the countMap.
+        for (int number : arr) {
+            // If the number is already in the map, increment its count, otherwise insert it with count 1.
+            countMap.merge(number, 1, Integer::sum);
+        }
+      
+        // Create a hashset containing all the values (occurrence counts) from the countMap.
+        Set<Integer> occurrenceSet = new HashSet<>(countMap.values());
+      
+        // If the size of the set (unique occurrences) is the same as the size of the map (unique numbers),
+        // it means that all occurrence counts are unique and we return true. Otherwise, return false.
+        return occurrenceSet.size() == countMap.size();
+    }
 }
