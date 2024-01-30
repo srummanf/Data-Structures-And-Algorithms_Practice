d5d17ba364296ff4f0b8d50f9f2e0ead19132afc
Rumman <rummanfardeen4567@gmail.com>
2024-01-30 21:19:43 +0530

    Stack Ops

--- Hashing/numSubmatrixSumTarget.java
+++ Hashing/numSubmatrixSumTarget.java
@@ -1,5 +1,5 @@
 /*
- * Given a matrix and a target, return the number of non-empty submatrices that sum to target.
+ Given a matrix and a target, return the number of non-empty submatrices that sum to target.
 
 A submatrix x1, y1, x2, y2 is the set of all cells matrix[x][y] with x1 <= x <= x2 and y1 <= y <= y2.
 
