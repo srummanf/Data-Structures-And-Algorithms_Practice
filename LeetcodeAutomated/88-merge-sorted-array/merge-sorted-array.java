dec76614f4fde62160d14acfa64c982d5080e244
Shaikh Rumman Fardeen <rummanfardeen4567@gmail.com>
2024-05-30 17:25:16 +0530

    Time: 1 ms (33.96%) | Memory: 41.9 MB (81.28%) - LeetSync

--- LeetcodeAutomated/88-merge-sorted-array/merge-sorted-array.java
+++ LeetcodeAutomated/88-merge-sorted-array/merge-sorted-array.java
@@ -5,7 +5,7 @@ class Solution {
         int i = 0, j = 0;
         ArrayList<Integer> al = new ArrayList<>();
         for (int k = 0; k < ss; k++) {
-            if (nums1[i] <= nums2[j]) {
+            if (nums1[i] < nums2[j]) {
                 al.add(nums1[i]);
                 i++;
             } else {
