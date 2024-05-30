9173f78c5046f4d0e454f928f88cd5fc76757a89
Shaikh Rumman Fardeen <rummanfardeen4567@gmail.com>
2024-05-30 16:46:36 +0530

    Time: 8 ms (87.04%) | Memory: 46.2 MB (84.98%) - LeetSync

---
+++ LeetcodeAutomated/56-merge-intervals/merge-intervals.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int[][] merge(int[][] arr) {
+        int n = arr.length; // size of the array
+        //sort the given intervals:
+        Arrays.sort(arr, new Comparator<int[]>() {
+            public int compare(int[] a, int[] b) {
+                return a[0] - b[0];
+            }
+        });
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) { // select an interval:
+            int start = arr[i][0];
+            int end = arr[i][1];
+
+            //Skip all the merged intervals:
+            if (!ans.isEmpty() && end <= ans.get(ans.size() - 1).get(1)) {
+                continue;
+            }
+
+            //check the rest of the intervals:
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j][0] <= end) {
+                    end = Math.max(end, arr[j][1]);
+                } else {
+                    break;
+                }
+            }
+            ans.add(Arrays.asList(start, end));
+        }
+        int s = ans.size();
+        int[][] res = new int [s][2];
+        for(int i = 0; i< s; i++){
+            int st = ans.get(i).get(0);
+            int ed = ans.get(i).get(1);
+            res[i] = new int[] {st,ed};
+        }
+        return res;
+        
+    }
+}
