66318985794e1fe4250a54bc282171a4055c0f0b
Shaikh Rumman Fardeen <rummanfardeen4567@gmail.com>
2024-06-04 16:02:35 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (39.95%) - LeetSync

--- LeetcodeAutomated/104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
+++ LeetcodeAutomated/104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
@@ -17,7 +17,5 @@ class Solution {
     public int maxDepth(TreeNode root) {
         if(root==null) return 0;
         return 1 + Math.max(maxDepth(root.left),maxDepth(root.right));
-        
-        
     }
 }
