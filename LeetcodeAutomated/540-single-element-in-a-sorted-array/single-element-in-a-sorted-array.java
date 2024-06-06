ec8c66ed53b5f41a2793b933b17f4b4bdfaaa5ae
Shaikh Rumman Fardeen <rummanfardeen4567@gmail.com>
2024-06-06 10:22:50 +0530

    Time: 0 ms (100.00%) | Memory: 50.5 MB (20.98%) - LeetSync

--- LeetcodeAutomated/540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ LeetcodeAutomated/540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,9 +1,25 @@
 class Solution {
+    // Naive solution
+    // public int singleNonDuplicate(int[] nums) {
+    // int xor=0;
+    // for(int i : nums){
+    // xor ^= i;
+    // }
+    // return xor;
+    // }
+
     public int singleNonDuplicate(int[] nums) {
-        int xor=0;
-        for(int i : nums){
-            xor ^= i;
+        int low = 0;
+        int high = nums.length-2;
+        while(low<=high){
+            int mid = (low + high) >> 1;
+            if(nums[mid] == nums[mid^1]){
+                low = mid+1;
+            } else {
+                high = mid -1;
+            }
+
         }
-        return xor;
+        return nums[low];
     }
 }
