0851b7f4b8fcf21ab4bab275b8639fd8dbfbd5bf
Shaikh Rumman Fardeen <rummanfardeen4567@gmail.com>
2024-06-08 08:26:10 +0530

    Time: 3 ms (10.88%) | Memory: 45.8 MB (27.05%) - LeetSync

--- LeetcodeAutomated/122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ LeetcodeAutomated/122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,48 +1,63 @@
 class Solution {
     static int profit = 0;
-    public static int f(int idx, int buy, int[] val, int[][] dp){
 
-        if(idx == val.length) return 0;
-        if(dp[idx][buy] != -1) return dp[idx][buy];
-        if(buy==1){
-            profit = Math.max((-val[idx] + f(idx+1, 0, val, dp)),(0 + f(idx+1, 1, val, dp)) );
+    public static int f(int idx, int buy, int[] val, int[][] dp) {
+
+        if (idx == val.length)
+            return 0;
+        if (dp[idx][buy] != -1)
+            return dp[idx][buy];
+        if (buy == 1) {
+            profit = Math.max((-val[idx] + f(idx + 1, 0, val, dp)), (0 + f(idx + 1, 1, val, dp)));
         } else {
-            profit = Math.max((val[idx] + f(idx+1, 1, val, dp)),(0 + f(idx+1, 0, val, dp)) );
+            profit = Math.max((val[idx] + f(idx + 1, 1, val, dp)), (0 + f(idx + 1, 0, val, dp)));
         }
 
         return dp[idx][buy] = profit;
 
     }
 
-    // public static int f_tab(int[] val){
-    //     int n = prices.length;
-    //     int[][] dp = new int [n+1][2];
+    public static int f_tab(int[] val) {
+        int n = val.length;
+        int[][] dp = new int[n + 1][2];
 
-    //     // Write for Base Case 
-    //     for(int i=0; i<=1; i++){
-    //         dp[n][i] = 0;
-    //     }
+        // Write for Base Case
+        for (int i = 0; i <= 1; i++) {
+            dp[n][i] = 0;
+        }
 
-    //     // Copy the memoization and use loops
-    //     // if(buy==1){
-    //     //     profit = Math.max((-val[idx] + f(idx+1, 0, val)),(0 + f(idx+1, 1, val)) );
-    //     // } else {
-    //     //     profit = Math.max((val[idx] + f(idx+1, 1, val)),(0 + f(idx+1, 0, val)) );
-    //     // }
+        // Copy the memoization and use loops
+        // if(buy==1){
+        // profit = Math.max((-val[idx] + f(idx+1, 0, val, dp)),(0 + f(idx+1, 1, val,
+        // dp)) );
+        // } else {
+        // profit = Math.max((val[idx] + f(idx+1, 1, val, dp)),(0 + f(idx+1, 0, val,
+        // dp)) );
+        // }
 
-    //     // return profit;
+        // return dp[idx][buy] = profit;
 
-    //     for(int idx = n-1; idx>=0 ; idx--){
-    //         for(int buy=0; buy<=1; buy++)
-    //     }
-    // }
+        for (int idx = n - 1; idx >= 0; idx--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                if (buy == 1) {
+                    profit = Math.max((-val[idx] + dp[idx + 1][0]), (0 + dp[idx + 1][1]));
+                } else {
+                    profit = Math.max((val[idx] + dp[idx + 1][1]), (0 + dp[idx + 1][0]));
+                }
+                dp[idx][buy] = profit;
+            }
+        }
+        return dp[0][1];
+    }
+
     public int maxProfit(int[] prices) {
         int n = prices.length;
         int[][] dp = new int[n][2];
-        for(int[] a : dp){
+        for (int[] a : dp) {
             Arrays.fill(a, -1);
         }
 
-        return f(0, 1, prices, dp );
+        // return f(0, 1, prices, dp);
+        return f_tab(prices);
     }
 }
