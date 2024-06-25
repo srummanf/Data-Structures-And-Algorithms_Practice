fc2cefb2434f518630fcde3aaa97e3524fdba5fe
Shaikh Rumman Fardeen <rummanfardeen4567@gmail.com>
2024-06-25 09:52:16 +0530

    Time: 8 ms (97.81%) | Memory: 44.4 MB (98.07%) - LeetSync

--- LeetcodeAutomated/455-assign-cookies/assign-cookies.java
+++ LeetcodeAutomated/455-assign-cookies/assign-cookies.java
@@ -1,23 +1,25 @@
+import java.util.Arrays;
+
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
-        // Sort the greed factors and cookie sizes
         Arrays.sort(g);
         Arrays.sort(s);
 
-        int childIndex = 0;
-        int cookieIndex = 0;
+        int count = 0;
+        int n = s.length;
+        int m = g.length;
 
-        // Iterate through the cookies and try to satisfy each child
-        while (childIndex < g.length && cookieIndex < s.length) {
-            if (s[cookieIndex] >= g[childIndex]) {
-                // If the current cookie can satisfy the current child, move to the next child
-                childIndex++;
+        int pos = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (pos < m && g[pos] <= s[i]) {
+                pos++;
+                count++;
             }
-            // Move to the next cookie in either case
-            cookieIndex++;
         }
 
-        // The number of children satisfied is the number of children we have iterated through
-        return childIndex;
+        return count;
     }
+
+
 }
