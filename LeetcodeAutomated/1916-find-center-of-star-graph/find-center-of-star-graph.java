45c9ed9363f6c0c2e676e3ca31315b3a28e6593f
Shaikh Rumman Fardeen <rummanfardeen4567@gmail.com>
2024-06-27 07:19:47 +0530

    Time: 13 ms (23.55%) | Memory: 88.1 MB (5.02%) - LeetSync

--- LeetcodeAutomated/1916-find-center-of-star-graph/find-center-of-star-graph.java
+++ LeetcodeAutomated/1916-find-center-of-star-graph/find-center-of-star-graph.java
@@ -1,51 +1,53 @@
+// import java.util.*;
+
+// class Solution {
+//     public int findCenter(int[][] edges) {
+//         // Since it's a star graph, we can use the fact that the center node will appear in all the edges
+//         // More specifically, it will appear in the first two edges for sure
+//         // We can use this property to determine the center node
+
+//         int node1 = edges[0][0];
+//         int node2 = edges[0][1];
+//         int node3 = edges[1][0];
+//         int node4 = edges[1][1];
+        
+//         if (node1 == node3 || node1 == node4) {
+//             return node1;
+//         } else {
+//             return node2;
+//         }
+//     }
+// }
+
+
 import java.util.*;
 
 class Solution {
     public int findCenter(int[][] edges) {
-        // Since it's a star graph, we can use the fact that the center node will appear in all the edges
-        // More specifically, it will appear in the first two edges for sure
-        // We can use this property to determine the center node
+        // Determine the number of nodes from the edges
+        int n = edges.length + 1;
 
-        int node1 = edges[0][0];
-        int node2 = edges[0][1];
-        int node3 = edges[1][0];
-        int node4 = edges[1][1];
-        
-        if (node1 == node3 || node1 == node4) {
-            return node1;
-        } else {
-            return node2;
+        // Initialize the adjacency list
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            adj.add(new ArrayList<>());
         }
+
+        // Populate the adjacency list
+        for (int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+
+        // Find the center node
+        for (int i = 1; i <= n; i++) {
+            if (adj.get(i).size() == n - 1) {
+                return i;
+            }
+        }
+
+        return -1; // If no center node is found
     }
 }
-
-
-// class Solution {
-//     public int findCenter(int[][] edges) {
-
-//         int r = edges.length;
-//         int c = edges[0].length;
-
-//         List<List<Integer>> adj = new ArrayList<>();
-
-//         for (int i = 0; i < r; i++) {
-//             adj.add(new ArrayList<>());
-//         }
-
-//         for (int i = 0; i < r; i++) {
-            
-//                 adj.get(edges[i][1]).add(edges[i][0]);
-//                 adj.get(edges[i][0]).add(edges[i][1]);
-            
-//         }
-
-//         for (int i = 0; i < r; i++){
-//             if(adj.get(i).size() == r){
-//                 return i;
-//             }
-//         }
-
-//         return -1;
-
-//     }
-// }
