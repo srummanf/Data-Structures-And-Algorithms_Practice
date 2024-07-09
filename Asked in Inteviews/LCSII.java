5315bff3278bf4c06af87dfc6e0e5e0d9ec7a979
Rumman <rummanfardeen4567@gmail.com>
2024-07-09 20:17:39 +0530

    substring and subsequence added

--- Asked in Inteviews/LCSII.java
+++ Asked in Inteviews/LCSII.java
@@ -1,50 +1,44 @@
 /** “You are given two strings, ‘s’ and ‘t,’ where ‘t’ is a subsequence of ‘s.’ Your task is to identify and remove the longest substring from ‘s’ such that ‘s’ still contains ‘t’ as a subsequence.”
  */
 
-import java.util.*;
+public class LCSII {
 
-class LCSII {
-    public static int ss(String s, String t) {
-        int slen = s.length();
-        int tlen = t.length();
-        boolean[] val = new boolean[slen];
-        Arrays.fill(val, false);
-        int pos = 0;
-        
-        for (int i = 0; i < slen; i++) {
-            if (s.charAt(i) == t.charAt(pos)) {
-                val[i] = true;
-                pos++;
-            }
-            if (pos == tlen) {
-                break;
-            }
-        }
-        
-        if (pos < tlen) {
-            return -1; // if `t` is not a subsequence of `s`
-        }
-        
-        int maxi = 0;
-        int curr = 0;
-        
-        for (int i = 0; i < slen; i++) {
-            if (!val[i]) {
-                curr++;
-            } else {
-                maxi = Math.max(maxi, curr);
-                curr = 0; // reset curr to 0 instead of 1
-            }
-        }
-        maxi = Math.max(maxi, curr);
-        
-        System.out.println(Arrays.toString(val));
-        return maxi;
-    }
+  public static void main(String[] args) {
+    String s = "abpcplea";
+    String t = "apple";
+    System.out.println(removeLongestSubstring(s, t));
+  }
 
-    public static void main(String[] args) {
-        String s = "abbbbbbbbbcde";
-        String t = "bcd";
-        System.out.println(ss(s, t)); // Output: 8
+  public static String removeLongestSubstring(String s, String t) {
+    int maxLength = 0;
+    String result = s;
+
+    // Iterate over all possible substrings
+    for (int i = 0; i < s.length(); i++) {
+      for (int j = i; j < s.length(); j++) {
+        String sub = s.substring(i, j + 1);
+        String modified = s.substring(0, i) + s.substring(j + 1);
+
+        if (isSubsequence(modified, t)) {
+          if (sub.length() > maxLength) {
+            maxLength = sub.length();
+            result = modified;
+          }
+        }
+      }
     }
+    return result;
+  }
+
+  public static boolean isSubsequence(String s, String t) {
+    int j = 0;
+    for (int i = 0; i < s.length() && j < t.length(); i++) {
+      if (s.charAt(i) == t.charAt(j)) {
+        j++;
+      }
+    }
+    return j == t.length();
+  }
+
+  public static void main(String[] args) {}
 }
