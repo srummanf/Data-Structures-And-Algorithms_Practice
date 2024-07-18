3b876e66c3c1022ea71e8479736eb7103046bf1a
Shaikh Rumman Fardeen <rummanfardeen4567@gmail.com>
2024-07-18 15:39:19 +0530

    Time: 5 ms (66.61%) | Memory: 45.3 MB (35.85%) - LeetSync

--- LeetcodeAutomated/207-course-schedule/course-schedule.java
+++ LeetcodeAutomated/207-course-schedule/course-schedule.java
@@ -1,42 +1,101 @@
 class Solution {
-
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
+        // Initialize the adjacency list
         for (int i = 0; i < numCourses; i++) {
             adj.add(new ArrayList<>());
         }
+
+        // Fill the adjacency list with prerequisites
         for (int[] prerequisite : prerequisites) {
             int course = prerequisite[0];
-            int prereq = prerequisite[1];
-            adj.get(prereq).add(course);
+            int prerequisiteCourse = prerequisite[1];
+            adj.get(prerequisiteCourse).add(course);
         }
-        int vis[] = new int[numCourses];
-        int dfsVis[] = new int[numCourses];
+
+        int[] topo = new int[numCourses];
+
+        int[] indegree = new int[numCourses];
 
         for (int i = 0; i < numCourses; i++) {
-            if (vis[i] == 0) {
-                if (checkCycle(i, adj, vis, dfsVis) == true)
-                    return false;
+            for (Integer destNode : adj.get(i)) {
+                indegree[destNode]++;
             }
         }
-        return true;
 
-    }
+        Queue<Integer> q = new LinkedList<Integer>();
 
-    private static boolean checkCycle(int node, ArrayList<ArrayList<Integer>> adj, int vis[], int dfsVis[]) {
-        vis[node] = 1;
-        dfsVis[node] = 1;
+        // Adding nodes to queue with indegree = 0
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                q.add(i);
+            }
+        }
 
-        for (Integer neighbor : adj.get(node)) {
-            if (vis[neighbor] == 0) {
-                if (checkCycle(neighbor, adj, vis, dfsVis) == true) {
-                    return true;
+        int cnt = 0;
+        int idx = 0;
+
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            topo[idx++] = node;
+            cnt++;
+            for (int ngh : adj.get(node)) {
+                indegree[ngh]--;
+                if (indegree[ngh] == 0) {
+                    q.add(ngh);
                 }
-            } else if (dfsVis[neighbor] == 1) {
-                return true;
             }
         }
-        dfsVis[node] = 0;
+
+        if(cnt==numCourses) return true;
         return false;
+
     }
 }
+
+// Solution 2
+
+// class Solution {
+
+// public boolean canFinish(int numCourses, int[][] prerequisites) {
+// ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+// for (int i = 0; i < numCourses; i++) {
+// adj.add(new ArrayList<>());
+// }
+// for (int[] prerequisite : prerequisites) {
+// int course = prerequisite[0];
+// int prereq = prerequisite[1];
+// adj.get(prereq).add(course);
+// }
+// int vis[] = new int[numCourses];
+// int dfsVis[] = new int[numCourses];
+
+// for (int i = 0; i < numCourses; i++) {
+// if (vis[i] == 0) {
+// if (checkCycle(i, adj, vis, dfsVis) == true)
+// return false;
+// }
+// }
+// return true;
+
+// }
+
+// private static boolean checkCycle(int node, ArrayList<ArrayList<Integer>>
+// adj, int vis[], int dfsVis[]) {
+// vis[node] = 1;
+// dfsVis[node] = 1;
+
+// for (Integer neighbor : adj.get(node)) {
+// if (vis[neighbor] == 0) {
+// if (checkCycle(neighbor, adj, vis, dfsVis) == true) {
+// return true;
+// }
+// } else if (dfsVis[neighbor] == 1) {
+// return true;
+// }
+// }
+// dfsVis[node] = 0;
+// return false;
+// }
+// }
