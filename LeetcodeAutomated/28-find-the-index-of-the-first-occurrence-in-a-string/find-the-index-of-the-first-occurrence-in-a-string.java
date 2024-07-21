8a94ecf38917787bf2d8b00da4cb9fcb182d9d1c
Shaikh Rumman Fardeen <rummanfardeen4567@gmail.com>
2024-07-21 16:05:10 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (90.29%) - LeetSync

---
+++ LeetcodeAutomated/28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int strStr(String str, String pat) {
+        int M = pat.length();
+        int N = str.length();
+
+        for (int i = 0; i <= N - M; i++) {
+            int j;
+            for (j = 0; j < M; j++)
+                if (str.charAt(i + j) != pat.charAt(j))
+                    break;
+
+            if (j == M)
+                return i;
+        }
+
+        return -1;
+    }
+}
