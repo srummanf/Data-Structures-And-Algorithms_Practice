7900acef6b953f917fd68225936ffe2a193358ec
Rumman <rummanfardeen4567@gmail.com>
2024-07-22 20:35:37 +0530

    Core Subject notes made 📝

--- Dynamic Programming/dp_45_LongestStringSequence.java
+++ Dynamic Programming/dp_45_LongestStringSequence.java
@@ -1,5 +1,67 @@
 //Day 7
 
+// Company Tags                : GOOGLE
+
+import java.util.Arrays;
+
+class Solution {
+
+    private boolean isPredecessor(String prev, String curr) {
+        int m = prev.length();
+        int n = curr.length();
+
+        if (n - m != 1 || m >= n)
+            return false;
+
+        int i = 0, j = 0;
+        // prev should be a subsequence of curr
+        while (i < m && j < n) {
+            if (prev.charAt(i) == curr.charAt(j)) {
+                i++;
+            }
+            j++;
+        }
+        return i == m;
+    }
+
+    public int modifiedLIS(String[] words, int prevIdx, int currIdx, int[][] dp) {
+        if (currIdx == words.length)
+            return 0;
+
+        if (prevIdx != -1 && dp[prevIdx][currIdx] != -1)
+            return dp[prevIdx][currIdx];
+
+        int take = 0;
+        if (prevIdx == -1 || isPredecessor(words[prevIdx], words[currIdx])) {
+            take = 1 + modifiedLIS(words, currIdx, currIdx + 1, dp);
+        }
+        int notTake = modifiedLIS(words, prevIdx, currIdx + 1, dp);
+
+        if (prevIdx != -1)
+            dp[prevIdx][currIdx] = Math.max(take, notTake);
+        return Math.max(take, notTake);
+    }
+
+    public int longestStrChain(String[] words) {
+        Arrays.sort(words, (a, b) -> Integer.compare(a.length(), b.length()));
+        int n = words.length;
+        int[][] dp = new int[1000 + 1][1000 + 1];
+        for (int[] row : dp)
+            Arrays.fill(row, -1);
+        return modifiedLIS(words, -1, 0, dp);
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String[] words = { "a", "ab", "ac", "bd", "abc", "abd", "abdd" };
+        System.out.println(solution.longestStrChain(words)); // Expected Output: 4
+    }
+}
+
+
+
+
+
 // https://leetcode.com/problems/longest-string-chain/description/
 
 import java.util.Arrays;
@@ -7,56 +69,50 @@ import java.util.Comparator;
 
 public class dp_45_LongestStringSequence {
 
-    // Arranging the array based on ascending length of the string length
-    public static String[] sortStringsByLength(String[] inputArray) {
-        String[] sortedArray = Arrays.copyOf(inputArray, inputArray.length);
-        Arrays.sort(sortedArray, Comparator.comparingInt(String::length));
-        return sortedArray;
+  // Arranging the array based on ascending length of the string length
+  public static String[] sortStringsByLength(String[] inputArray) {
+    String[] sortedArray = Arrays.copyOf(inputArray, inputArray.length);
+    Arrays.sort(sortedArray, Comparator.comparingInt(String::length));
+    return sortedArray;
+  }
+
+  // Comparing two strings --> 2 pointer Appraoch
+  boolean compare(String s1, String s2) {
+    if (s1.length() != s2.length() + 1) return false;
+
+    int first = 0;
+    int second = 0;
+
+    while (first < s1.length()) {
+      if (second < s2.length() && (s1.charAt(first) == s2.charAt(second))) {
+        first++;
+        second++;
+      } else first++;
     }
+    if (
+      first == s1.length() && second == s2.length()
+    ) return true; else return false;
+  }
 
-    // Comparing two strings --> 2 pointer Appraoch
-    boolean compare(String s1, String s2) {
-        if (s1.length() != s2.length() + 1)
-            return false;
+  // Tabulation DP
+  public int longestStrChain(String[] words) {
+    int n = words.length;
+    int maxi = 1;
 
-        int first = 0;
-        int second = 0;
+    String[] arr = sortStringsByLength(words);
 
-        while (first < s1.length()) {
-            if (second < s2.length() && (s1.charAt(first) == s2.charAt(second))) {
-                first++;
-                second++;
-            } else
-                first++;
+    int[] dp = new int[n];
+    Arrays.fill(dp, 1);
+
+    for (int i = 0; i <= n - 1; i++) {
+      for (int prev_index = 0; prev_index <= i - 1; prev_index++) {
+        if (compare(arr[i], arr[prev_index]) && (1 + dp[prev_index] > dp[i])) {
+          dp[i] = 1 + dp[prev_index];
         }
-        if (first == s1.length() && second == s2.length())
-            return true;
-        else
-            return false;
+      }
+
+      if (dp[i] > maxi) maxi = dp[i];
     }
-
-    // Tabulation DP
-    public int longestStrChain(String[] words) {
-        int n = words.length;
-        int maxi = 1;
-
-        String[] arr = sortStringsByLength(words);
-
-        int[] dp = new int[n];
-        Arrays.fill(dp, 1);
-
-        for (int i = 0; i <= n - 1; i++) {
-
-            for (int prev_index = 0; prev_index <= i - 1; prev_index++) {
-
-                if (compare(arr[i], arr[prev_index]) && (1 + dp[prev_index] > dp[i])) {
-                    dp[i] = 1 + dp[prev_index];
-                }
-            }
-
-            if (dp[i] > maxi)
-                maxi = dp[i];
-        }
-        return maxi;
-    }
+    return maxi;
+  }
 }
--- String/Knuth_Morris_Pratt_Algo_PatternMatching.java
+++ String/Knuth_Morris_Pratt_Algo_PatternMatching.java
@@ -1,4 +1,3 @@
-
 class Knuth_Morris_Pratt_Algo_PatternMatching {
 
   public static void Patternsearch(String str, String pat) {
@@ -6,14 +5,14 @@ class Knuth_Morris_Pratt_Algo_PatternMatching {
     int N = str.length();
 
     for (int i = 0; i <= N - M; i++) {
-      int j;
+      int j;    
       for (j = 0; j < M; j++) if (str.charAt(i + j) != pat.charAt(j)) break;
 
       if (j == M) System.out.println("Pattern found at index " + i);
     }
   }
 
-  public static void main(String[] args){
+  public static void main(String[] args) {
     Patternsearch("abcabcabc", "abc");
   }
 }
