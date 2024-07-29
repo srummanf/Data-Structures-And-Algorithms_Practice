834ffe89f417a2818648aa4e2bb91fa49a976f94
Shaikh Rumman Fardeen <rummanfardeen4567@gmail.com>
2024-07-29 20:06:39 +0530

    Time: 26 ms (41.35%) | Memory: 54.3 MB (5.06%) - LeetSync

--- LeetcodeAutomated/1511-count-number-of-teams/count-number-of-teams.java
+++ LeetcodeAutomated/1511-count-number-of-teams/count-number-of-teams.java
@@ -1,28 +1,112 @@
 class Solution {
+    // Function to build the segment tree
+    void buildSegmentTree(int i, int l, int r, int[] segmentTree, int[] arr) {
+        if (l == r) {
+            segmentTree[i] = arr[l];
+            return;
+        }
+        int mid = l + (r - l) / 2;
+        buildSegmentTree(2 * i + 1, l, mid, segmentTree, arr);
+        buildSegmentTree(2 * i + 2, mid + 1, r, segmentTree, arr);
+        segmentTree[i] = segmentTree[2 * i + 1] + segmentTree[2 * i + 2];
+    }
+
+    // Function to update the segment tree
+    void updateSegmentTree(int idx, int val, int i, int l, int r, int[] segmentTree, int[] arr) {
+        if (l == r) {
+            segmentTree[i] = val;
+            arr[idx] = val;
+            return;
+        }
+
+        int mid = l + (r - l) / 2;
+
+        if (idx <= mid) {
+            updateSegmentTree(idx, val, 2 * i + 1, l, mid, segmentTree, arr);
+        } else {
+            updateSegmentTree(idx, val, 2 * i + 2, mid + 1, r, segmentTree, arr);
+        }
+
+        segmentTree[i] = segmentTree[2 * i + 1] + segmentTree[2 * i + 2];
+    }
+
+    // Function to query the segment tree
+    int querySegmentTree(int start, int end, int i, int l, int r, int[] segmentTree) {
+        if (l > end || r < start) {
+            return 0;
+        }
+
+        if (l >= start && r <= end) {
+            return segmentTree[i];
+        }
+
+        int mid = l + (r - l) / 2;
+        return querySegmentTree(start, end, 2 * i + 1, l, mid, segmentTree) +
+               querySegmentTree(start, end, 2 * i + 2, mid + 1, r, segmentTree);
+    }
+
     public int numTeams(int[] rating) {
         int n = rating.length;
+        int[] lessLeft = new int[n];
+        int[] greaterLeft = new int[n];
+        int[] lessRight = new int[n];
+        int[] greaterRight = new int[n];
+        int[] segmentTree = new int[4 * 100001]; // Since rating values can be up to 100000
+        int[] arr = new int[100001];
         int count = 0;
 
-        for (int j = 1; j < n - 1; j++) {
-            int lessLeft = 0, greaterLeft = 0;
-            int lessRight = 0, greaterRight = 0;
+        // Count elements less and greater on the left side
+        for (int i = 0; i < n; i++) {
+            lessLeft[i] = querySegmentTree(0, rating[i] - 1, 0, 0, 100000, segmentTree);
+            greaterLeft[i] = querySegmentTree(rating[i] + 1, 100000, 0, 0, 100000, segmentTree);
+            updateSegmentTree(rating[i], arr[rating[i]] + 1, 0, 0, 100000, segmentTree, arr);
+        }
 
-            for (int i = 0; i < j; i++) {
-                if (rating[i] < rating[j]) lessLeft++;
-                if (rating[i] > rating[j]) greaterLeft++;
-            }
+        // Reset the segment tree
+        segmentTree = new int[4 * 100001];
+        arr = new int[100001];
 
-            for (int k = j + 1; k < n; k++) {
-                if (rating[k] < rating[j]) lessRight++;
-                if (rating[k] > rating[j]) greaterRight++;
-            }
+        // Count elements less and greater on the right side
+        for (int i = n - 1; i >= 0; i--) {
+            lessRight[i] = querySegmentTree(0, rating[i] - 1, 0, 0, 100000, segmentTree);
+            greaterRight[i] = querySegmentTree(rating[i] + 1, 100000, 0, 0, 100000, segmentTree);
+            updateSegmentTree(rating[i], arr[rating[i]] + 1, 0, 0, 100000, segmentTree, arr);
+        }
 
-            count += lessLeft * greaterRight + greaterLeft * lessRight;
+        // Calculate the number of valid teams
+        for (int i = 0; i < n; i++) {
+            count += lessLeft[i] * greaterRight[i] + greaterLeft[i] * lessRight[i];
         }
 
         return count;
     }
 }
+
+// class Solution {
+//     public int numTeams(int[] rating) {
+//         int n = rating.length;
+//         int count = 0;
+
+//         for (int j = 1; j < n - 1; j++) {
+//             int lessLeft = 0, greaterLeft = 0;
+//             int lessRight = 0, greaterRight = 0;
+
+//             for (int i = 0; i < j; i++) {
+//                 if (rating[i] < rating[j]) lessLeft++;
+//                 if (rating[i] > rating[j]) greaterLeft++;
+//             }
+
+//             for (int k = j + 1; k < n; k++) {
+//                 if (rating[k] < rating[j]) lessRight++;
+//                 if (rating[k] > rating[j]) greaterRight++;
+//             }
+
+//             count += lessLeft * greaterRight + greaterLeft * lessRight;
+//         }
+
+//         return count;
+//     }
+// }
 
 
 // class Solution {
