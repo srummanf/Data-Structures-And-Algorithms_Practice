1a91105bf0f5c29d3685b55bca6f26c99bcb2261
Shaikh Rumman Fardeen <rummanfardeen4567@gmail.com>
2024-08-03 20:08:02 +0530

    Time: 22 ms (38.48%) | Memory: 52.5 MB (8.43%) - LeetSync

--- LeetcodeAutomated/966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ LeetcodeAutomated/966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -2,8 +2,8 @@ import java.util.HashMap;
 
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
-        HashMap<Integer, Integer> prefixSumCount = new HashMap<>();
-        prefixSumCount.put(0, 1); // There's one way to get a sum of 0: by having an empty subarray
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        hm.put(0, 1); // There's one way to get a sum of 0: by having an empty subarray
         
         int currentSum = 0;
         int result = 0;
@@ -12,12 +12,12 @@ class Solution {
             currentSum += num; // Update the running sum
             
             // Check if there is a subarray with sum equal to `goal` ending at this index
-            if (prefixSumCount.containsKey(currentSum - goal)) {
-                result += prefixSumCount.get(currentSum - goal);
+            if (hm.containsKey(currentSum - goal)) {
+                result += hm.get(currentSum - goal);
             }
             
             // Update the prefix sum count map
-            prefixSumCount.put(currentSum, prefixSumCount.getOrDefault(currentSum, 0) + 1);
+            hm.put(currentSum, hm.getOrDefault(currentSum, 0) + 1);
         }
         
         return result;
