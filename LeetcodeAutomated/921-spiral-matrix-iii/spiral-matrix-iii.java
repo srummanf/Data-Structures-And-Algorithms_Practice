73a458c0ba688379cadebb254fc0f8ef0e9b62c8
Shaikh Rumman Fardeen <rummanfardeen4567@gmail.com>
2024-08-08 15:07:35 +0530

    Time: 5 ms (47.43%) | Memory: 45.1 MB (72.43%) - LeetSync

--- LeetcodeAutomated/921-spiral-matrix-iii/spiral-matrix-iii.java
+++ LeetcodeAutomated/921-spiral-matrix-iii/spiral-matrix-iii.java
@@ -1,31 +1,35 @@
 class Solution {
     public int[][] spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
-        int[][] ans = new int[rows * cols][2]; // Correct size of the result array
-        int[][] direction = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } }; // E, S, W, N
-        int steps = 1; // Initial steps in a direction
-        int dir = 0; // Direction index
-        int idx_x = 0; // Index for filling ans
+        int[][] directions = {
+            {0, 1},  // EAST
+            {1, 0},  // SOUTH
+            {0, -1}, // WEST
+            {-1, 0}  // NORTH
+        };
+        
+        int[][] result = new int[rows * cols][2];  
+        int step = 0; // how many steps to move
+        int dir  = 0; // which direction
 
-        ans[idx_x][0] = rStart;
-        ans[idx_x][1] = cStart;
-        idx_x++;
+        result[0] = new int[]{rStart, cStart};
+        int count = 1;
 
-        while (idx_x < rows * cols) {
-            for (int i = 0; i < 2; i++) { // Increase steps every two directions (E and W or N and S)
-                for (int j = 0; j < steps; j++) {
-                    rStart += direction[dir][0];
-                    cStart += direction[dir][1];
-                    if (rStart >= 0 && rStart < rows && cStart >= 0 && cStart < cols) {
-                        ans[idx_x][0] = rStart;
-                        ans[idx_x][1] = cStart;
-                        idx_x++;
-                    }
+        while (count < rows * cols) {
+            // When we move EAST or WEST, we need to increase our steps by 1
+            if (dir == 0 || dir == 2) step++;
+
+            for (int i = 0; i < step; i++) {
+                rStart += directions[dir][0];
+                cStart += directions[dir][1];
+
+                if (rStart >= 0 && rStart < rows && cStart >= 0 && cStart < cols) { 
+                    result[count++] = new int[]{rStart, cStart};
                 }
-                dir = (dir + 1) % 4; // Change direction
             }
-            steps++; // Increment steps after completing a full cycle of directions
-        }
 
-        return ans;
+            dir = (dir + 1) % 4; 
+        }
+        
+        return result;
     }
 }
