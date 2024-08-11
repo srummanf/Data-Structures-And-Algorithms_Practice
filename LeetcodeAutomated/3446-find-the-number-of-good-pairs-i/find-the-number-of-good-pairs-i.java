ddd71fb96f5d260d1ebce0ebe2150047be8ff692
Shaikh Rumman Fardeen <rummanfardeen4567@gmail.com>
2024-08-11 11:30:57 +0530

    Time: 1 ms (99.72%) | Memory: 43.3 MB (6.45%) - LeetSync

--- LeetcodeAutomated/3446-find-the-number-of-good-pairs-i/find-the-number-of-good-pairs-i.java
+++ LeetcodeAutomated/3446-find-the-number-of-good-pairs-i/find-the-number-of-good-pairs-i.java
@@ -1,17 +1,15 @@
 class Solution {
     public int numberOfPairs(int[] nums1, int[] nums2, int k) {
-        int count = 0;
-        int n = nums1.length;
-        int m = nums2.length;
-        
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (nums1[i] % (nums2[j] * k) == 0) {
-                    count++;
+        int ans=0;
+        for(int i=0;i<nums1.length;i++){
+             if(nums1[i] % k != 0)
+                continue;
+            for(int j=0;j<nums2.length;j++){
+                if(nums1[i] % (nums2[j]*k)==0){
+                    ans++;
                 }
             }
         }
-        
-        return count;
+        return ans;
     }
 }
