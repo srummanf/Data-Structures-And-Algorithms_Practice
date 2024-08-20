03ec7dd0ebe4b6a96f9bbc6e620e4e3787bb28ef
Shaikh Rumman Fardeen <rummanfardeen4567@gmail.com>
2024-08-20 16:29:48 +0530

    Time: 2704 ms (6.25%) | Memory: 40.2 MB (47.28%) - LeetSync

--- LeetcodeAutomated/96-unique-binary-search-trees/unique-binary-search-trees.java
+++ LeetcodeAutomated/96-unique-binary-search-trees/unique-binary-search-trees.java
@@ -1,22 +1,19 @@
 // Catalan Numbers
 
 class Solution {
-    public int f(int[] dp, int n) {
+    public int f(int n) {
         if (n <= 1)
             return 1;
-        if (dp[n] != -1)
-            return dp[n];
         int ans = 0;
         for (int i = 1; i <= n; i++) {
-            ans += f(dp, i - 1) * f(dp, n - i);
+            ans += f(i - 1) * f(n - i);
         }
         return ans;
     }
 
     public int numTrees(int n) {
-        int[] dp = new int[n + 1];
-        Arrays.fill(dp, -1);
-        return f(dp, n);
+
+        return f(n);
 
     }
 }
