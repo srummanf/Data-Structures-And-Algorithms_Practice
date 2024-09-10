e039bca37ed9ffa9f4c9e28da36f1600f70ec864
Shaikh Rumman Fardeen <rummanfardeen4567@gmail.com>
2024-09-10 19:58:02 +0530

    Time: 2 ms (71.11%) | Memory: 45.1 MB (44.58%) - LeetSync

--- LeetcodeAutomated/2903-insert-greatest-common-divisors-in-linked-list/insert-greatest-common-divisors-in-linked-list.java
+++ LeetcodeAutomated/2903-insert-greatest-common-divisors-in-linked-list/insert-greatest-common-divisors-in-linked-list.java
@@ -6,38 +6,74 @@ class Solution {
             return gcd(b, a % b);
     }
 
-    public ListNode insert(int val, ListNode root) {
-        if (root == null)
-            return new ListNode(val);
-        ListNode curr = root;
-        while (curr.next != null) {
-            curr = curr.next;
-        }
-        curr.next = new ListNode(val);
-        return root;
-    }
-
     public ListNode insertGreatestCommonDivisors(ListNode head) {
         if (head == null || head.next == null) {
             return head;
         }
 
         ListNode curr = head;
-        ListNode ans = null;
 
-        while (curr.next != null) {
+        while (curr != null && curr.next != null) {
             int a = curr.val;
             int b = curr.next.val;
             int gcd_val = gcd(a, b);
 
-            ans = insert(a, ans);        
-            ans = insert(gcd_val, ans);  
+            // Create a new node with the GCD value
+            ListNode gcdNode = new ListNode(gcd_val);
 
-            curr = curr.next;            
+            // Insert this new node between curr and curr.next
+            gcdNode.next = curr.next;
+            curr.next = gcdNode;
+
+            // Move to the next pair
+            curr = gcdNode.next;
         }
 
-        ans = insert(curr.val, ans);     // Insert the last node value
-
-        return ans;
+        return head;
     }
 }
+
+
+// class Solution {
+//     public int gcd(int a, int b) {
+//         if (b == 0)
+//             return a;
+//         else
+//             return gcd(b, a % b);
+//     }
+
+//     public ListNode insert(int val, ListNode root) {
+//         if (root == null)
+//             return new ListNode(val);
+//         ListNode curr = root;
+//         while (curr.next != null) {
+//             curr = curr.next;
+//         }
+//         curr.next = new ListNode(val);
+//         return root;
+//     }
+
+//     public ListNode insertGreatestCommonDivisors(ListNode head) {
+//         if (head == null || head.next == null) {
+//             return head;
+//         }
+
+//         ListNode curr = head;
+//         ListNode ans = null;
+
+//         while (curr.next != null) {
+//             int a = curr.val;
+//             int b = curr.next.val;
+//             int gcd_val = gcd(a, b);
+
+//             ans = insert(a, ans);        
+//             ans = insert(gcd_val, ans);  
+
+//             curr = curr.next;            
+//         }
+
+//         ans = insert(curr.val, ans);    
+
+//         return ans;
+//     }
+// }
