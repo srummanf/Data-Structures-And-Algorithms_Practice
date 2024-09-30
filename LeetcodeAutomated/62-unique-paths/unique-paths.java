bbaa8dcbd802ad20ec7f08fddd912a702d90c019
Shaikh Rumman Fardeen <rummanfardeen4567@gmail.com>
2024-09-30 15:35:47 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (27.09%) - LeetSync

--- LeetcodeAutomated/62-unique-paths/unique-paths.java
+++ LeetcodeAutomated/62-unique-paths/unique-paths.java
@@ -1,22 +1,20 @@
 class Solution {
-
-    public int f(int i, int j, int m, int n, int[][] dp) {
-        if (i < 0 || j < 0)
-            return 0;
-        if (i == 0 && j == 0)
-            return 1;
-        if (dp[i][j] != -1)
-            return dp[i][j];
-        int up = f(i - 1, j, m, n, dp);
-        int left = f(i, j - 1, m, n, dp);
-        return dp[i][j] = up + left;
-
-    }
-
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m + 1][n + 1];
-        for (int[] a : dp)
-            Arrays.fill(a, -1);
-        return f(m - 1, n - 1, m, n, dp);
+        for (int[] i : dp)
+            Arrays.fill(i, -1);
+        return f(m, n, m - 1, n - 1, dp);
+    }
+
+    public int f(int m, int n, int i, int j, int[][] dp) {
+        if (i == 0 && j == 0)
+            return 1;
+        if (i < 0 || j < 0)
+            return 0;
+        if (dp[i][j] != -1)
+            return dp[i][j];
+        int up = f(m, n, i - 1, j, dp);
+        int left = f(m, n, i, j - 1, dp);
+        return dp[i][j] = up + left;
     }
 }
