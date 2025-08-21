83f66985e565b26ce2c4a5e6d011ff9593f94cef
Shaikh Rumman Fardeen <rummanfardeen4567@gmail.com>
2025-08-21 17:15:29 +0530

    Time: 6 ms (23.38%) | Memory: 44.8 MB (85.82%) - LeetSync

--- LeetcodeAutomated/345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
+++ LeetcodeAutomated/345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
@@ -1,6 +1,43 @@
 import java.util.*;
 
 class Solution {
+    Set<Character> vowels = new HashSet<>(Arrays.asList(
+        'a','e','i','o','u',
+        'A','E','I','O','U'
+    ));
+
+    // Swap helper for char array
+    private void swap(char[] arr, int i, int j) {
+        char temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public String reverseVowels(String s) {
+        char[] charArray = s.toCharArray();
+        int l = 0, r = charArray.length - 1;
+
+        while (l < r) {
+            if (!vowels.contains(charArray[l])) {
+                l++;
+            } else if (!vowels.contains(charArray[r])) {
+                r--;
+            } else {
+                swap(charArray, l, r);
+                l++;
+                r--;
+            }
+        }
+
+        return new String(charArray);
+    }
+}
+
+
+/**
+import java.util.*;
+
+class Solution {
     public String reverseVowels(String s) {
         Stack<Character> st = new Stack<>();
         Set<Character> vowels = new HashSet<>(Arrays.asList(
@@ -28,3 +65,5 @@ class Solution {
         return ans.toString();
     }
 }
+
+ */
